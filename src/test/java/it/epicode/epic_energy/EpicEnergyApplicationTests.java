61ac2d78a368173d821ba24498eae170fa45f926
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2025-01-20 10:10:49 +0100

    Initial commit

---
+++ src/test/java/it/epicode/epic_energy/EpicEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.epic_energy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
